a04dc4b031a4c0837e738f60f887ce5de0d00347
Dade Huang <dadehuang@gmail.com>
2018-11-18 20:21:39 -0800

    commit on 11-18-2018

---
+++ src/TestJavaFX/AlertBoxTest.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TestJavaFX;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class AlertBoxTest extends Application {
+
+    Stage window;
+    Button button;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        window = primaryStage;
+        window.setTitle("thenewboston");
+        button = new Button("Click Me");
+
+        button.setOnAction(e -> AlertBox.display("Title of Window", "Wow this alert box is awesome!"));
+
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+        Scene scene = new Scene(layout, 300, 250);
+        window.setScene(scene);
+        window.show();
+    }
+
+}
